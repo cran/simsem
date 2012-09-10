03d6889a382564faefaf523ebfe091c3894e674a
Sunthud Pornprasertmanit <psunthud@ku.edu>
2012-09-10 00:00:00 +0000

    version 0.3-5

--- R/findPower.R
+++ R/findPower.R
@@ -1,5 +1,5 @@
-# findPower: Find a value of a given independent variable that provides a given value of power. This function can handle multiple
-# ivs by split data.
+# findPower: Find a value of a given independent variable that provides a given
+# value of power. This function can handle multiple ivs by split data.
 
 findPower <- function(powerTable, iv, power) {
     ivCol <- grep("iv", colnames(powerTable))
@@ -33,8 +33,32 @@ findPower <- function(powerTable, iv, power) {
     return(pow)
 }
 
-# findTargetPower: Find a value of a given independent variable that provides a given value of power. This function can handle only
-# one independent variable.
+# findTargetPower: Find a value of a given independent variable that provides a
+# given value of power. This function can handle only one independent variable.
+
+# \title{
+	# Find a value of varying parameters that provides a given value of power. 
+# }
+# \description{
+	# Find a value of varying parameters that provides a given value of power. This function can deal with only one varying parameter only (\code{\link{findPower}} can deal with more than one varying parameter).
+# }
+# \usage{
+# findTargetPower(iv, dv, power)
+# }
+# \arguments{
+  # \item{iv}{
+	# A vector of the target varying parameter
+# }
+  # \item{dv}{
+	# A \code{data.frame} of the power table of target parameters
+# }
+  # \item{power}{
+	# A desired power.
+# }
+# }
+# \value{
+	# The value of the target varying parameter providing the desired power. If the value is \code{NA}, there is no value in the domain of varying parameters that provide the target power. If the value is the minimum value of the varying parameters, it means that the minimum value has already provided enough power. The value of varying parameters that provides exact desired power may be lower than the minimum value.
+# }
 
 findTargetPower <- function(iv, dv, power) {
     FUN <- function(dv, iv, power) {
---
+++ R/validate.R
@@ -0,0 +1,116 @@
+# validateObject: Validate whether the drawn parameters are good (providing an identified model).
+
+# arguments: paramSet A target set of parameters
+
+# value: Return \code{TRUE} if the target parameters are good.
+
+validateObject <- function(paramSet) {
+    if (validateCovariance(paramSet$VPS, paramSet$RPS, paramSet$VE) == FALSE) 
+        return(FALSE)
+    if (!is.null(paramSet$BE)) {
+        # Path or SEM
+        if (validatePath(paramSet$BE, paramSet$VE, paramSet$VE) == FALSE) 
+            return(FALSE)
+    }
+    if (!is.null(paramSet$LY)) {
+        # SEM or CFA
+        if (validateCovariance(paramSet$VTE, paramSet$RTE, paramSet$VY) == FALSE) 
+            return(FALSE)
+        if (validatePath(paramSet$LY, paramSet$VE, paramSet$VY) == FALSE) 
+            return(FALSE)
+    }
+    return(TRUE)
+}
+
+# validatePath: Validate whether the regression coefficient (or loading) matrix
+# is good
+
+# arguments: a) path A regression coefficient or loading matrix
+# 			 b) var.iv The variances of variables corresponding to the columns
+# 			 c) var.dv The variances of variables corresponding to the rows
+
+# value: Return \code{TRUE} if the target regression coefficient matrix is good.
+
+validatePath <- function(path, var.iv, var.dv) {
+    # Need to account for multiple independent variables
+    if (isTRUE(all.equal(var.iv, round(var.iv)))) 
+        var.iv <- round(var.iv)
+    if (isTRUE(all.equal(var.dv, round(var.dv)))) 
+        var.dv <- round(var.dv)
+    singleIV <- apply(path, 1, function(object) {
+        x <- object[!is.na(object)]
+        sum(x != 0) == 1
+    })
+    if (all(singleIV == 0)) {
+        return(TRUE)
+    } else {
+        path <- extract(path, row = which(singleIV))
+        var.dv <- var.dv[singleIV]
+        inv.var.iv <- 1/var.iv
+        max.path <- sqrt(var.dv) %o% sqrt(inv.var.iv)
+        abs.path <- abs(path)
+        max.path[var.dv == 0, ] <- abs.path[var.dv == 0, ]
+        ifelse(sum(abs.path > max.path) > 0, return(FALSE), return(TRUE))
+    }
+}
+
+# validateCovariance: Validate whether all elements provides a good covariance matrix
+
+# arguments: a) resVar A vector of residual variances
+# 			b) correlation A correlation matrix
+# 			c) totalVar A vector of total variances
+
+# value: Return \code{TRUE} if the covariance matrix is good
+
+validateCovariance <- function(resVar, correlation, totalVar = NULL) {
+    if (!isSymmetric(correlation)) 
+        return(FALSE)
+    if (sum(resVar < 0) > 0) 
+        return(FALSE)
+    zero.row <- resVar == 0
+    if (sum(zero.row) > 0) {
+        target.rows <- extract(correlation, row = which(zero.row), col = NULL)
+        for (i in 1:nrow(target.rows)) {
+            temp <- target.rows[i, -zero.row[i]]
+            if (sum(temp != 0) > 0) 
+                return(FALSE)
+        }
+        if (det(correlation < 0)) 
+            return(FALSE)
+    } else {
+        if (det(correlation) <= 0) 
+            return(FALSE)
+    }
+    if (!is.null(totalVar)) {
+        if (sum(totalVar < 0) > 0) 
+            return(FALSE)
+    }
+    return(TRUE)
+}
+
+# Extract: extract elements from a matrix without changing to a vector
+
+# arguments: a) object a target matrix
+#			b) row a target row
+#			c) col a target column
+
+# value: The resulting matrix with extracted elements
+
+extract <- function(object, row = NULL, col = NULL) {
+    if (class(object) == "matrix") {
+        if (is.null(row)) 
+            row <- 1:nrow(object)
+        if (is.null(col)) 
+            col <- 1:ncol(object)
+        if (length(row) > 1 & length(col) > 1) {
+            return(object[row, col])
+        } else {
+            if (length(col) == 1) 
+                return(as.matrix(object[row, col]))
+            if (length(row) == 1) 
+                return(t(as.matrix(object[row, col])))
+        }
+    } else {
+        stop("This function can be used for a matrix only.")
+    }
+} 
